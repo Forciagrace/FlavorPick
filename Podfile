fa3880e3938ae0125819dbe1e78182f94388e71f
forcia.grace <forcia.grace@zohocorp.com>
2018-02-13 13:00:07 +0530

    Initial Commit

---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios, '8.0'
+use_frameworks!
+
+target 'FlavorPick' do
+pod 'Alamofire', '4.4.0'
+end
---
+++ Pods/Target Support Files/Pods-FlavorPick/Pods-FlavorPick-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FlavorPick : NSObject
+@end
+@implementation PodsDummy_Pods_FlavorPick
+@end
