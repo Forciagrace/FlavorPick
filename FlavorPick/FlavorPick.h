347db0581f89f10a56c48d6caf79346134537240
forcia.grace <forcia.grace@zohocorp.com>
2018-02-13 12:26:57 +0530

    Initial Commit

---
+++ FlavorPick/FlavorPick.h
@@ -0,0 +1,19 @@
+//
+//  FlavorPick.h
+//  FlavorPick
+//
+//  Created by Forcia Grace on 13/02/18.
+//  Copyright © 2018 Forcia Grace. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FlavorPick.
+FOUNDATION_EXPORT double FlavorPickVersionNumber;
+
+//! Project version string for FlavorPick.
+FOUNDATION_EXPORT const unsigned char FlavorPickVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FlavorPick/PublicHeader.h>
+
+
